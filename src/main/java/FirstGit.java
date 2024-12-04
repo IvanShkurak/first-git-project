47e36c8d4a6009a06fc103aae4d576cf982cb588
IvanShkurak <ivanshkurak7@gmail.com>
2024-12-04 18:39:01 +0300

    Deleted sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git develop");
         System.out.println("Git learning!");
-        System.out.println("Im very Happy");
         System.out.println("Ivan");
         System.out.println("is me");
     }
