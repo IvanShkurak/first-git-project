4b8f1f6152bea38599cae0e4151cb53b764ec4b8
IvanShkurak <ivanshkurak7@gmail.com>
2024-12-04 18:36:15 +0300

    Changed sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("Git develop");
         System.out.println("Git learning!");
-        System.out.println("Im Happy");
+        System.out.println("Im very Happy");
         System.out.println("Ivan");
         System.out.println("is me");
     }
